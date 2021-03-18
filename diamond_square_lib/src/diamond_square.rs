2e6b63eac6f6184e2d10ac794d617c2ece81a7a6
Kurt Strecker <ksphotopro@gmail.com>
2021-03-17 23:41:24 -0400

    initial commit

---
+++ diamond_square_lib/src/diamond_square.rs
@@ -0,0 +1,4 @@
+pub fn create_ds(dimension: u32) -> u32 {
+  println!("{}", dimension);
+  dimension
+}
---
+++ test_app_diamond_square/src/main.rs
@@ -0,0 +1,7 @@
+extern crate diamond_square_lib;
+use diamond_square_lib::diamond_square::create_ds;
+
+fn main() {
+    println!("Main Test");
+    create_ds(100);
+}
