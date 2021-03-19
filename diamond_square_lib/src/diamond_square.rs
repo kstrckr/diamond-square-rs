749fd41ade590403fb559faf972d8b834a92612e
kstrckr <ksphotopro@gmail.com>
2021-03-18 23:35:45 -0400

    rudimentary rng clamp and attenuation over rounds

--- diamond_square_lib/src/diamond_square.rs
+++ diamond_square_lib/src/diamond_square.rs
@@ -1,20 +1,25 @@
   // const DIMENSION: u8 = 16385;
-use rand::prelude::*;
+  use std::{thread, time};
+  use rand::prelude::*;
 
 const TWO: u32 = 2;
 
-const N: u32 = 10;
-const DIMENSION: usize = TWO.pow(N) as usize + 1;
+// const N: u32 = 14;
+// const DIMENSION: usize = 16385;
+
+const N: u32 = 8;
+const DIMENSION: usize = 257;
 
 pub fn create_ds() -> [u32; DIMENSION] {
-  generate_ds()
+  generate_ds(50)
 }
 
-pub fn generate_ds() -> [u32; DIMENSION] {
+pub fn generate_ds(upper_bound: u32) -> [u32; DIMENSION] {
+  // randomness baseline
   let mut rng = thread_rng();
   let mut array: [u32; DIMENSION] = [0; DIMENSION];
-  array[0] = rng.gen_range(0, 1000);
-  array[DIMENSION - 1] = rng.gen_range(0, 1000);
+  array[0] = rng.gen_range(0, upper_bound);
+  array[DIMENSION - 1] = rng.gen_range(0, upper_bound);
 
   let highest_index = TWO.pow(N);
 
@@ -30,11 +35,23 @@ pub fn generate_ds() -> [u32; DIMENSION] {
         let target_index = (highest_index / pow) * j;
         let lower_source = target_index - index_offset;
         let upper_source = target_index + index_offset;
-        array[target_index as usize] = (&array[lower_source as usize] + &array[upper_source as usize]) / 2;
+        array[target_index as usize] = ((&array[lower_source as usize] + &array[upper_source as usize]) / 2) + rng.gen_range(0, upper_bound / 2);
       }
     }
-    println!("{:?}", array);
   }
-
   array
+}
+
+pub fn print_midpoint_displaced_array(arr: [u32; DIMENSION]) {
+  // println!("{:?}", arr);
+  let ten_millis = time::Duration::from_millis(10);
+  let max: &u32 = arr.iter().max().unwrap();
+  for i in 0..DIMENSION {
+    let mut printed_row: String = String::new();
+    for j in 0..arr[i] {
+      printed_row.push('*');
+    }
+    println!("{}", printed_row);
+    thread::sleep(ten_millis);
+  }
 }
--- test_app_diamond_square/src/main.rs
+++ test_app_diamond_square/src/main.rs
@@ -1,8 +1,8 @@
 extern crate diamond_square_lib;
-use diamond_square_lib::diamond_square::create_ds;
+use diamond_square_lib::diamond_square;
 
 fn main() {
     println!("Main Test");
-    let generated_data = create_ds();
-    println!("{:?}", generated_data);
+    let generated_data = diamond_square::create_ds();
+    diamond_square::print_midpoint_displaced_array(generated_data);
 }
