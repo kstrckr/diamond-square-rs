dce251a861b5cf99b7e663a0d843d7f0940d437d
Kurt Strecker <ksphotopro@gmail.com>
2021-03-20 19:35:26 -0400

    very smooth looking rng now

--- diamond_square_lib/src/diamond_square.rs
+++ diamond_square_lib/src/diamond_square.rs
@@ -15,18 +15,22 @@ pub fn create_ds() -> [u32; DIMENSION] {
 }
 
 pub fn generate_ds(upper_bound: u32) -> [u32; DIMENSION] {
-  // randomness baseline
+  // TODO: randomness baseline
+
+  // ERROR CHECKING
+  let highest_index = TWO.pow(N);
+  if highest_index + 1 != DIMENSION as u32 {
+    panic!("N {} and Dimension {} are incorrect", N, DIMENSION)
+  }
+
+  // initial setup
   let mut rng = thread_rng();
   let mut array: [u32; DIMENSION] = [0; DIMENSION];
   array[0] = rng.gen_range(0, upper_bound);
   array[DIMENSION - 1] = rng.gen_range(0, upper_bound);
 
-  let highest_index = TWO.pow(N);
 
-  if highest_index + 1 != DIMENSION as u32 {
-    panic!("N {} and Dimension {} are incorrect", N, DIMENSION)
-  }
-
+  // generate the data
   for i in 1..N + 1 {
     let pow = TWO.pow(i);
     for j in 1..pow {
@@ -35,7 +39,7 @@ pub fn generate_ds(upper_bound: u32) -> [u32; DIMENSION] {
         let target_index = (highest_index / pow) * j;
         let lower_source = target_index - index_offset;
         let upper_source = target_index + index_offset;
-        array[target_index as usize] = ((&array[lower_source as usize] + &array[upper_source as usize]) / 2) + rng.gen_range(0, (upper_bound - i) / 2);
+        array[target_index as usize] = ((&array[lower_source as usize] + &array[upper_source as usize]) / 2) + rng.gen_range(0, (upper_bound - i) / i);
       }
     }
   }
@@ -43,9 +47,7 @@ pub fn generate_ds(upper_bound: u32) -> [u32; DIMENSION] {
 }
 
 pub fn print_midpoint_displaced_array(arr: [u32; DIMENSION]) {
-  // println!("{:?}", arr);
   let ten_millis = time::Duration::from_millis(10);
-  let max: &u32 = arr.iter().max().unwrap();
   for i in 0..DIMENSION {
     let mut printed_row: String = String::new();
     for j in 0..arr[i] {
